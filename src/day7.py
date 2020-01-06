a82ad95547b2d9f327dd44c9e08e4ebcafbf41bb
Tenzin Chan <tenzin_chan@mymail.sutd.edu.sg>
2020-01-06 08:33:08 +0000

    Updated day7's IntcodeComputer to match cleaned up version of day 5

--- src/day7.py
+++ src/day7.py
@@ -1,51 +1,60 @@
+from copy import copy
 from itertools import permutations
 from src.ops7 import *
 
 class IntcodeComputer:
     def __init__(self, instructions):
-        self.memory = instructions
-        self.instructions = instructions
-        self.pc = 0
-        self.input_ptr = 0
+        self.instructions = copy(instructions)
+        self.memory = copy(instructions)
 
     def reinit(self):
-        self.memory = self.instructions
-        self.pc = 0
-        self.input_ptr = 0
+        self.memory = copy(self.instructions)
 
     def run(self, inp=None):
         self.inp = inp
-        while self.pc < len(self.memory):
+        inp_ptr = 0
+        pc = 0
+        while pc < len(self.memory):
             try:
-                self.try_exec()
+                pc, inp_ptr = self.try_exec(pc, inp_ptr)
             except HaltException:
                 break
         return self.memory
 
-    def try_exec(self):
-        opcode = self.memory[self.pc]
+    def try_exec(self, pc, inp_ptr):
+        opcode = self.memory[pc]
+        ops = OPS_NUM_ARGS.keys()
         argmodes, op = opcode // 100, opcode % 100
         if op == HALT_CODE:
             raise HaltException
-        if op in OPS:
-            self.exec(op, argmodes)
+        elif op in ops:
+            return self.exec(op, argmodes, pc, inp_ptr)
         else:
             raise InvalidOpException
 
-    def exec(self, op, argmodes):
+    def exec(self, op, argmodes, pc, inp_ptr):
         num_args = OPS_NUM_ARGS[op]
-        args = self.memory[self.pc+1:self.pc+1+num_args]
+        args = self.memory[pc+1:pc+1+num_args]
+        pc += 1 + num_args
         if op == 1:
             self.two_arg_store(*args, argmodes, add)
         elif op == 2:
             self.two_arg_store(*args, argmodes, mult)
         elif op == 3:
-            inp = self.inp[self.input_ptr]
+            inp = self.inp[inp_ptr]
+            inp_ptr += 1
             self.mem_store(*args, inp)
-            self.input_ptr += 1
-        else: #4
+        elif op == 4:
             self.out = self.mem_load(*args)
-        self.pc += 1 + num_args
+        elif op == 5:
+            pc = self.jump(*args, argmodes, pc, neq_zero)
+        elif op == 6:
+            pc = self.jump(*args, argmodes, pc, eq_zero)
+        elif op == 7:
+            self.two_arg_store(*args, argmodes, lt)
+        elif op == 8:
+            self.two_arg_store(*args, argmodes, eq)
+        return pc, inp_ptr
 
     def arg_values(self, argmodes, *args):
         decoded_args = []
@@ -55,6 +64,10 @@ class IntcodeComputer:
             else:
                 decoded_args.append(arg)
         return decoded_args
+
+    def jump(self, a_loc, b_loc, argmodes, pc, jump_to):
+        a, b = self.arg_values(argmodes, a_loc, b_loc)
+        return jump_to(a, b, pc)
 
     def two_arg_store(self, a_loc, b_loc, loc, argmodes, func):
         a, b = self.arg_values(argmodes, a_loc, b_loc)
@@ -66,7 +79,6 @@ class IntcodeComputer:
 
     def mem_store(self, mem_loc, value):
         self.check_mem_inbound(mem_loc)
-        assert type(value) == int, 'value must be an int'
         self.memory[mem_loc] = value
 
     def check_mem_inbound(self, mem_loc):
--- src/ops7.py
+++ src/ops7.py
@@ -6,16 +6,34 @@ class InvalidOpException(Exception):
 
 HALT_CODE = 99
 
-OPS = (1, 2, 3, 4)
-
 OPS_NUM_ARGS = {1 : 3,
         2 : 3,
         3 : 1,
-        4 : 1}
+        4 : 1,
+        5 : 2,
+        6 : 2,
+        7 : 3,
+        8 : 3}
 
 def add(a, b):
     return a + b
 
 def mult(a, b):
     return a * b
+
+def neq_zero(a, b, c):
+    if a != 0:
+        return b
+    return c
+
+def eq_zero(a, b, c):
+    if a == 0:
+        return b
+    return c
+
+def lt(a, b):
+    return int(a < b)
+
+def eq(a, b):
+    return int(a == b)
 
